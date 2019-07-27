61603a3c5dcd87f378e84de2473168380622d926
jetalai3 <jetalai3@gmail.com>
2019-07-27 12:07:42 +0200

    feat: add timeline component to personal page

--- src/containers/PersonalPage/PersonalPage.js
+++ src/containers/PersonalPage/PersonalPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Avatar from '../../components/Avatar/Avatar'
 import Description from '../../components/Description/Descriptipon';
+import AuthorsTimeline from '../../components/Timeline/AuthorsTimeline'
 
 import data from '../../data/authors';
 
@@ -11,7 +12,7 @@ function PersonalPage(props) {
         <div className="personal-page">
             <Avatar data={profile}/>
             <Description data={profile}/>
-
+            <AuthorsTimeline activities = {profile.activity}/>
         </div>
     );
 }
