b64bc5ae99e42edb9a5dfdbb00b0633ec259d7e7
Roman Petrukhin <natescorp@mail.ru>
2019-07-28 15:24:05 +0300

    feat: implementation Material UI to DevPage

--- src/components/Description/Descriptipon.js
+++ src/components/Description/Descriptipon.js
@@ -2,16 +2,6 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
 function Description(props) {
-    const activity = props.data.activity.map((item) => {
-        return (
-            <div>
-                <hr/>
-                <p> {item.date} </p>
-                <p> {item.place} </p>
-                <p> {item.description} </p>
-            </div>
-        );
-    });
     return (
         <div className="description">
             <Typography variant="h4" component="h3">
@@ -26,11 +16,6 @@ function Description(props) {
             <Typography component="p" variant="body1">
                 Умер: <span>{props.data.deathDate}</span> <i>{props.data.deathPlace}</i>
             </Typography>
-            <Typography component="p" variant="body1">
-                Деятельность:
-                {activity}
-            </Typography>
-
         </div>
     )
 }
--- src/components/PersonalInfo/PersonalInfo.js
+++ src/components/PersonalInfo/PersonalInfo.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Typography from "@material-ui/core/Typography";
 
 function PersonalInfo(props) {
     return (
         <div >
-            <p>{props.data.name}</p>
-            <p><a href={props.data.gitLink} target="_blank">GitHub</a></p>
-            <p><a href={props.data.mailLink} target="_blank">EMAIL: {props.data.mailLink}</a></p>
+            <Typography variant="h5" component="h4">
+                <p>{props.data.name}</p>
+            </Typography>
+            <Typography component="p" variant="subtitle2">
+                <p><a href={props.data.gitLink} target="_blank">GitHub</a></p>
+            </Typography>
+            <Typography component="p" variant="subtitle2">
+                <p><a href={props.data.mailLink} target="_blank">EMAIL: {props.data.mailLink}</a></p>
+            </Typography>
         </div>
     )
 }
