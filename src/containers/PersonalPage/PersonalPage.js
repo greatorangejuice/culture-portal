e56bb538046331dd926602a380a1a3dd2efd9f0c
felcillive <belofelcita@gmail.com>
2019-07-31 22:51:48 +0300

    refactor: move photo's information in separate file

--- src/containers/PersonalPage/PersonalPage.js
+++ src/containers/PersonalPage/PersonalPage.js
@@ -4,12 +4,14 @@ import Description from '../../components/Description/Descriptipon';
 import Container from '@material-ui/core/Container';
 import AuthorsTimeline from '../../components/Timeline/AuthorsTimeline';
 import data from '../../data/authors';
+import dataPhoto from '../../data/gallery';
 import Video from '../../components/Video/Video';
 import Gallery from '../../components/Gallery/Gallery';
 import WorkList from '../../components/WorkList/WorkList';
 
 function PersonalPage(props) {
   const profile = data[props.person];
+  const photo = dataPhoto[props.person];
   return (
     <Container maxWidth="md">
       <div className="personal-page">
@@ -17,7 +19,7 @@ function PersonalPage(props) {
         <Description data={profile} />
         <AuthorsTimeline activities={profile.activity} />
         <Video data={profile} />
-        <Gallery data={profile} />
+        <Gallery data={photo} />
         <WorkList data={profile} />
       </div>
     </Container>
--- src/data/authors.js
+++ src/data/authors.js
@@ -6,48 +6,6 @@ import avatar_4 from '../img/avatars/Gilevich_NS.jpg';
 import avatar_5 from '../img/avatars/Zuenok_VV.jpg';
 import avatar_6 from '../img/avatars/Kullinkovich_AM.jpg';
 
-import poet1_1 from '../img/gallery/1.1.jpg';
-import poet1_2 from '../img/gallery/1.2.jpg';
-import poet1_3 from '../img/gallery/1.3.jpg';
-import poet1_4 from '../img/gallery/1.4.jpg';
-import poet1_5 from '../img/gallery/1.5.jpg';
-import poet1_6 from '../img/gallery/1.6.jpg';
-import poet1_7 from '../img/gallery/1.7.jpg';
-import poet1_8 from '../img/gallery/1.8.jpg';
-import poet1_9 from '../img/gallery/1.9.jpg';
-import poet1_10 from '../img/gallery/1.10.jpg';
-
-import poet2_1 from '../img/gallery/2.1.jpg';
-import poet2_2 from '../img/gallery/2.2.jpg';
-import poet2_3 from '../img/gallery/2.3.jpg';
-import poet2_4 from '../img/gallery/2.4.jpg';
-import poet2_5 from '../img/gallery/2.5.jpg';
-import poet2_6 from '../img/gallery/2.6.jpg';
-
-import poet3_1 from '../img/gallery/3.1.jpg';
-import poet3_2 from '../img/gallery/3.2.jpg';
-import poet3_3 from '../img/gallery/3.3.jpg';
-import poet3_4 from '../img/gallery/3.4.jpg';
-import poet3_5 from '../img/gallery/3.5.jpg';
-import poet3_6 from '../img/gallery/3.6.jpg';
-
-import poet4_1 from '../img/gallery/4.1.jpg';
-import poet4_2 from '../img/gallery/4.2.jpg';
-import poet4_3 from '../img/gallery/4.3.jpg';
-import poet4_4 from '../img/gallery/4.4.jpg';
-import poet4_5 from '../img/gallery/4.5.jpg';
-import poet4_6 from '../img/gallery/4.6.jpg';
-
-import poet5_1 from '../img/gallery/5.1.jpg';
-import poet5_2 from '../img/gallery/5.2.jpg';
-import poet5_3 from '../img/gallery/5.3.jpg';
-import poet5_4 from '../img/gallery/5.4.jpg';
-
-import poet6_1 from '../img/gallery/6.1.jpg';
-import poet6_2 from '../img/gallery/6.2.jpg';
-import poet6_3 from '../img/gallery/6.3.jpg';
-import poet6_4 from '../img/gallery/6.4.jpg';
-
 export default [
   {
     id: 1,
@@ -109,49 +67,6 @@ export default [
         date: '02.03.2014',
         place: 'Минск',
         description: 'Умер 2 марта 2014 года в своей квартире в Минске на 80-ом году жизни от болезни Паркинсона.',
-      },
-    ],
-    imgData: [
-      {
-        img: poet1_1,
-        title: 'В день получения университетского диплома.1959 год',
-      },
-      {
-        img: poet1_2,
-        title: 'С дочерью Илоной. 1967 год',
-      },
-      {
-        img: poet1_3,
-        title: 'Начинающие литераторы с Якубом Коласом. Бородулин стоит слева от Коласа. 1955 год',
-      },
-      {
-        img: poet1_4,
-        title: 'С Максимом Танком (слева). Выступление в Туве. 1964 год',
-      },
-      {
-        img: poet1_5,
-        title: 'С Владимиром Короткевичем и собакой Дозором. Королищевичи. 1965 год',
-      },
-      {
-        img: poet1_6,
-        title: 'На родине родителей в деревне Ковалевщина. 1972 год',
-      },
-      {
-        img: poet1_7,
-        title: 'Первокурсники на картошке. Деревня Новый Двор Минского района. 1954 год',
-      },
-      {
-        img: poet1_8,
-        title: 'Первокурсники на картошке. Деревня Новый Двор Минского района. 1954 год',
-      },
-      {
-        img: poet1_9,
-        title:
-          'В университетском скверике. Рыгор Бородулин, Анатолий Клышко, Василий Зуенок, Анатолий Вертинский, Геннадь Буравкин. Сидят: Юрась Свирка, Микола Арочка, Кастусь Цвирка.',
-      },
-      {
-        img: poet1_10,
-        title: 'На военных сборах. Анатолий Ставров, Алесь Булыка, Рыгор Бородулин',
       },
     ],
   },
@@ -247,32 +162,6 @@ export default [
         description: 'П. У. Бровка умер 24 марта 1980 года в Минске. Похоронен на Восточном кладбище.',
       },
     ],
-    imgData: [
-      {
-        img: poet2_1,
-        title: 'Петрусь Бровка с Василём Быковым',
-      },
-      {
-        img: poet2_2,
-        title: 'Петрусь Бровка',
-      },
-      {
-        img: poet2_3,
-        title: 'Бровка с Еленой Михайловной Ридевской',
-      },
-      {
-        img: poet2_4,
-        title: 'П. Броўка (сядзіць крайні справа) з удзельнікамі нарады маладых пісьменнікаў (верасень 1955).',
-      },
-      {
-        img: poet2_5,
-        title: 'Сцяпан Сямашка, Паўлюк Шукайла, Пятрусь Броўка, Янка Відук (Скрыган). Полацак, 1927 г',
-      },
-      {
-        img: poet2_6,
-        title: 'П. Броўка (4-ты справа) на III з’ездзе пісьменнікаў БССР (1954)',
-      },
-    ],
   },
   {
     id: 3,
@@ -329,32 +218,6 @@ export default [
         date: '03.11.1941',
         place: 'Уральск',
         description: 'Скончался около Уральска в Казахстане, где и был похоронен',
-      },
-    ],
-    imgData: [
-      {
-        img: poet3_1,
-        title: 'Змитрок Бядуля',
-      },
-      {
-        img: poet3_2,
-        title: 'Змитрок Бядуля с женой ',
-      },
-      {
-        img: poet3_3,
-        title: `Літаратурнае аб'яднанне «Узвышша». З. Бядуля ў цэнтры. 1929`,
-      },
-      {
-        img: poet3_4,
-        title: 'Змітрок Бядуля і Я. Колас у дзень узнагароджання ордэнамі. 1939',
-      },
-      {
-        img: poet3_5,
-        title: 'Якуб Колас, Янка Купала, Пятрусь Броўка, Зьмітрок Бядуля 1930-я гг.',
-      },
-      {
-        img: poet3_6,
-        title: 'Змітрок Бядуля і Якуб Колас (у цэнтры) сярод студэнтаў рабфака БДУ, 1922 г.   ',
       },
     ],
   },
@@ -438,32 +301,6 @@ export default [
         description: 'Скончался в Минске от онкологического заболевания.',
       },
     ],
-    imgData: [
-      {
-        img: poet4_1,
-        title: 'Нил Гилевич и Василий Быков',
-      },
-      {
-        img: poet4_2,
-        title: 'Змитрок Бядуля с женой ',
-      },
-      {
-        img: poet4_3,
-        title: 'Нил Гилевич в Белорусском государственном университете имени В.И. Ленина, 1977 год.',
-      },
-      {
-        img: poet4_4,
-        title: 'С Максимом Танком, 1977 год.',
-      },
-      {
-        img: poet4_5,
-        title: 'Нил Гилевич, Анатолий Вертинский, Геннадий Буравкин, Владимир Анискович в редакции "Літаратура і Мастацтва". 1979 год.',
-      },
-      {
-        img: poet4_6,
-        title: 'Нил Гилевич разрезает ленточку на открытии музея белорусского поэта Максима Богдановича, 1987 год.',
-      },
-    ],
   },
   {
     id: 5,
@@ -537,24 +374,6 @@ export default [
         description: 'Председатель Союза писателей Беларуси.',
       },
     ],
-    imgData: [
-      {
-        img: poet5_1,
-        title: 'Василий Зуенок поздравляет Кондрата Крапиву',
-      },
-      {
-        img: poet5_2,
-        title: 'Василий Зуенок и Микола Ахрамчик ',
-      },
-      {
-        img: poet5_3,
-        title: 'С Рыгором Бородулиным и Эдуардом Доморацким',
-      },
-      {
-        img: poet5_4,
-        title: 'С Геннадием Буравкиным',
-      },
-    ],
   },
   {
     id: 6,
@@ -616,24 +435,6 @@ export default [
           'Причиной смерти называют проблемы с лёгкими, точнее «скрытое воспаление лёгких». В день ' +
           'смерти музыканта группа Neuro Dubel должна была выступать на фестивале «Рок за Бобров». ' +
           'Прощание прошло 5 августа в зале крематория на Северном кладбище Минска.',
-      },
-    ],
-    imgData: [
-      {
-        img: poet6_1,
-        title: 'Александр Куллинкович, лидер группы Neuro Dubel',
-      },
-      {
-        img: poet6_2,
-        title: 'Neuro Dubel',
-      },
-      {
-        img: poet6_3,
-        title: '',
-      },
-      {
-        img: poet6_4,
-        title: '',
       },
     ],
   },
