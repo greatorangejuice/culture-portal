7abae8dbf804dd93f09ce0d0883832f55d57a625
greatorangejuice <bypavelsnigirev@gmail.com>
2019-08-03 22:38:00 +0300

    feat: add developer photo

--- src/data/developers.js
+++ src/data/developers.js
@@ -1,7 +1,7 @@
 import avatar_1 from '../img/developersPhotos/RomanPetrikhin.jpg'
 import avatar_2 from '../img/developersPhotos/YanaKorduba.jpg'
 import avatar_3 from '../img/developersPhotos/KartasauIlya.jpg'
-import avatar_4 from '../img/developersPhotos/KartasauIlya.jpg'
+import avatar_4 from '../img/developersPhotos/ViktarSvintsitski.png'
 import avatar_5 from '../img/developersPhotos/PavelSnigirev.jpg'
 
 export default [
@@ -29,7 +29,7 @@ export default [
     {
         id: 4,
         name: 'Viktar Svintsitski',
-        avatar: avatar_3,
+        avatar: avatar_4,
         gitLink: 'https://github.com/jetalai3',
         mailLink: 'jetalai3@gmail.com',
     },
